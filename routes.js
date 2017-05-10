57c7d55d9a2d28fb294f2ea31d16cb5253fef0f6
lolim10 <khalid>
2017-05-10 21:17:59 +0100

    first push

---
+++ routes.js
@@ -0,0 +1,132 @@
+var express = require("express");
+var User = require("./models/user");
+var passport = require("passport");
+
+var router = express.Router();
+
+
+//?????????????????????
+router.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	res.locals.errors = req.flash("error");
+	res.locals.infos = req.flash("info");
+	next();
+});
+
+
+
+//route for main page 
+router.get("/", function(req, res, next) {
+	User.find()
+	.sort({ createdAt: "descending" })
+	.exec(function(err, users) {
+		if (err) { return next(err); }
+		res.render("index", { users: users });
+	});
+});
+
+
+//route for GET signup 
+router.get("/signup", function(req, res) {
+	res.render("signup");
+});
+
+//route for POST signup
+router.post("/signup", function(req, res, next) {
+	var username = req.body.username;
+	var password = req.body.password;
+
+	User.findOne({ username: username }, function(err, user) {
+		if (err) { return next(err); }
+		if (user) {
+			req.flash("error", "User already exists");
+			return res.redirect("/signup");
+		}
+	
+		var newUser = new User({
+			username: username,
+			password: password
+		});
+		newUser.save(next);
+	});
+	}, 
+	passport.authenticate("login", {
+		successRedirect: "/",
+		failureRedirect: "/signup",
+		failureFlash: true
+	})
+);
+
+
+//GET login
+router.get("/login", function(req, res) {
+	res.render("login");
+});
+
+
+//POST login
+router.post("/login", passport.authenticate("login", {
+	successRedirect: "/",
+	failureRedirect: "/login",
+	failureFlash: true
+}));
+
+
+//GET logout
+router.get("/logout", function(req, res) {
+	req.logout();
+	res.redirect("/");
+});
+
+
+
+//GET edit
+router.get("/edit", ensureAuthenticated, function(req, res) {
+	res.render("edit");
+});
+
+
+
+//POST edit (normally a PUT request but browsers supportonly get and post) 
+router.post("/edit", ensureAuthenticated, function(req, res, next) {
+	req.user.displayName = req.body.displayname;
+	req.user.bio = req.body.bio;
+	req.user.save(function(err) {
+		if (err) { next(err); return;}
+		req.flash("info", "Profile updated!");
+		res.redirect("/edit");
+	});
+});
+
+
+
+
+
+
+// GET users
+router.get("/users/:username", function(req, res, next) {
+	User.findOne({ username: req.params.username }, function(err, user) {
+		if (err) { return next(err); }
+		if (!user) { return next(404); }
+		res.render("profile", { user: user });
+	});
+});
+
+
+
+// to make sure user is authenticated (used in the GET edit route)
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		next();
+	} else {
+		req.flash("info", "You must be logged in to see this page.");
+		res.redirect("/login");
+	}
+}
+
+
+
+
+
+//exporting routes
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var flash = require("connect-flash");
+var routes = require("./routes");
+
+var passport = require("passport");
+var setUpPassport = require("./setuppassport");
+
+
+var app = express();
+
+mongoose.connect("mongodb://localhost:27017/test");
+app.set("port", process.env.PORT || 3000);
+
+setUpPassport();
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());// handling cookies
+
+
+app.use(session({
+	secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+	resave: true,
+	saveUninitialized: true
+}));
+
+
+app.use(flash());// for showing errors
+
+app.use(passport.initialize());//handling sessions and stuff
+app.use(passport.session());
+
+
+app.use(routes);
+
+app.listen(app.get("port"), function() {
+	console.log("Magic happening on port " + app.get("port"));
+});
